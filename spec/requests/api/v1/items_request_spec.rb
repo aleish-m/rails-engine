e058ac6ba90c4ee9f3a5473a726303b3db80d2bf
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-18 20:34:32 -0600

    Test write test for all items endpoint and one item endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,71 @@
 require 'rails_helper'
 
 describe "Items API" do
-  it "sends a list of all items" do
-    create_list(:item, 5)
+  describe 'happy paths' do
+    it "sends a list of all items" do
+      x = create_list(:item, 5)
+
+      get '/api/v1/items'
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(5)
+
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to be_a(String)
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+
+        expect(item).to_not have_key(:created_at)
+        expect(item).to_not have_key(:updated_at)
+      end
+    end
+
+    it 'can get a single item by its id' do
+      id = create(:item).id
+
+      get "/api/v1/items/#{id}"
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(item).to have_key(:data)
+      expect(item[:data]).to be_a(Hash)
+
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_a(String)
+
+      expect(item[:data]).to have_key(:type)
+      expect(item[:data][:type]).to be_a(String)
+
+      expect(item[:data]).to have_key(:attributes)
+      expect(item[:data][:attributes]).to be_a(Hash)
+
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+
+      expect(item[:data]).to_not have_key(:created_at)
+      expect(item[:data]).to_not have_key(:updated_at)
+    end
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "Merchant API" do
+describe 'Merchant API' do
   describe 'happy paths' do
-    it "sends a list of all merchants" do
+    it 'sends a list of all merchants' do
       create_list(:merchant, 4)
-      
+
       get '/api/v1/merchants'
 
       expect(response).to be_successful
-      
+
       merchants = JSON.parse(response.body, symbolize_names: true)
 
       expect(merchants[:data].count).to eq(4)
@@ -22,7 +22,7 @@ describe "Merchant API" do
 
         expect(merchant).to have_key(:attributes)
         expect(merchant[:attributes]).to be_a(Hash)
-        
+
         expect(merchant[:attributes]).to have_key(:name)
         expect(merchant[:attributes][:name]).to be_a(String)
 
@@ -31,7 +31,7 @@ describe "Merchant API" do
       end
     end
 
-    it "can get a single merchant by its id" do
+    it 'can get a single merchant by its id' do
       id = create(:merchant).id
 
       get "/api/v1/merchants/#{id}"
@@ -51,7 +51,7 @@ describe "Merchant API" do
 
       expect(merchant[:data]).to have_key(:attributes)
       expect(merchant[:data][:attributes]).to be_a(Hash)
-        
+
       expect(merchant[:data][:attributes]).to have_key(:name)
       expect(merchant[:data][:attributes][:name]).to be_a(String)
 
@@ -59,14 +59,14 @@ describe "Merchant API" do
       expect(merchant[:data]).to_not have_key(:updated_at)
     end
 
-    it "can get all items for a given merchant ID" do
+    it 'can get all items for a given merchant ID' do
       id = create(:merchant).id
       items = create_list(:item, 5, merchant_id: id)
 
       get "/api/v1/merchants/#{id}/items"
 
       merchant = JSON.parse(response.body, symbolize_names: true)
-      
+
       expect(response).to be_successful
 
       expect(merchant).to have_key(:data)
@@ -101,8 +101,8 @@ describe "Merchant API" do
   end
 
   describe 'sad paths' do
-    it "returns a empty merchant data hash when a invalid merchant id is requested" do
-      get "/api/v1/merchants/1"
+    it 'returns a empty merchant data hash when a invalid merchant id is requested' do
+      get '/api/v1/merchants/1'
 
       merchant = JSON.parse(response.body, symbolize_names: true)
 
@@ -125,11 +125,10 @@ describe "Merchant API" do
 
       expect(merchant[:error][:attributes]).to have_key(:name)
       expect(merchant[:error][:attributes][:name]).to be_a(String)
-
     end
 
-    it "returns a empty merchant items data hash when a invalid merchant id is requested" do
-      get "/api/v1/merchants/1/items"
+    it 'returns a empty merchant items data hash when a invalid merchant id is requested' do
+      get '/api/v1/merchants/1/items'
 
       merchant = JSON.parse(response.body, symbolize_names: true)
 
@@ -161,7 +160,6 @@ describe "Merchant API" do
 
       expect(merchant[:error][0][:attributes]).to have_key(:merchant_id)
       expect(merchant[:error][0][:attributes][:merchant_id]).to be(nil)
-
     end
   end
 end
