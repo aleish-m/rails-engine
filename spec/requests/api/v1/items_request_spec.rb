d88446a0ed89a0f47fb76f832ea1b790bcce2e15
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-19 16:02:20 -0600

    Test: Add sad path test for item indext endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,208 +1,284 @@
 require 'rails_helper'
 
 describe 'Items API' do
-  describe 'happy paths' do
-    it 'sends a list of all items' do
-      create_list(:item, 5)
+  describe 'item index' do
+    describe 'happy path' do
+      it 'sends a list of all items' do
+        create_list(:item, 5)
 
-      get '/api/v1/items'
+        get '/api/v1/items'
 
-      expect(response).to be_successful
+        expect(response).to be_successful
 
-      items = JSON.parse(response.body, symbolize_names: true)
+        items = JSON.parse(response.body, symbolize_names: true)
 
-      expect(items[:data].count).to eq(5)
+        expect(items[:data].count).to eq(5)
 
-      items[:data].each do |item|
-        expect(item).to have_key(:id)
-        expect(item[:id]).to be_a(String)
+        items[:data].each do |item|
+          expect(item).to have_key(:id)
+          expect(item[:id]).to be_a(String)
 
-        expect(item).to have_key(:type)
-        expect(item[:type]).to be_a(String)
+          expect(item).to have_key(:type)
+          expect(item[:type]).to be_a(String)
 
-        expect(item).to have_key(:attributes)
-        expect(item[:attributes]).to be_a(Hash)
+          expect(item).to have_key(:attributes)
+          expect(item[:attributes]).to be_a(Hash)
 
-        expect(item[:attributes]).to have_key(:name)
-        expect(item[:attributes][:name]).to be_a(String)
+          expect(item[:attributes]).to have_key(:name)
+          expect(item[:attributes][:name]).to be_a(String)
 
-        expect(item[:attributes]).to have_key(:description)
-        expect(item[:attributes][:description]).to be_a(String)
+          expect(item[:attributes]).to have_key(:description)
+          expect(item[:attributes][:description]).to be_a(String)
 
-        expect(item[:attributes]).to have_key(:unit_price)
-        expect(item[:attributes][:unit_price]).to be_a(Float)
+          expect(item[:attributes]).to have_key(:unit_price)
+          expect(item[:attributes][:unit_price]).to be_a(Float)
 
-        expect(item[:attributes]).to have_key(:merchant_id)
-        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+          expect(item[:attributes]).to have_key(:merchant_id)
+          expect(item[:attributes][:merchant_id]).to be_a(Integer)
 
-        expect(item).to_not have_key(:created_at)
-        expect(item).to_not have_key(:updated_at)
+          expect(item).to_not have_key(:created_at)
+          expect(item).to_not have_key(:updated_at)
+        end
       end
     end
 
-    it 'can get a single item by its id' do
-      id = create(:item).id
+    describe 'sad path' do
+      it 'sends a empty list of items if there are no items' do
+        get '/api/v1/items'
 
-      get "/api/v1/items/#{id}"
+        expect(response).to be_successful
 
-      item = JSON.parse(response.body, symbolize_names: true)
+        items = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_successful
+        expect(items[:data].count).to eq(0)
 
-      expect(item).to have_key(:data)
-      expect(item[:data]).to be_a(Hash)
-
-      expect(item[:data]).to have_key(:id)
-      expect(item[:data][:id]).to be_a(String)
-
-      expect(item[:data]).to have_key(:type)
-      expect(item[:data][:type]).to be_a(String)
-
-      expect(item[:data]).to have_key(:attributes)
-      expect(item[:data][:attributes]).to be_a(Hash)
-
-      expect(item[:data][:attributes]).to have_key(:name)
-      expect(item[:data][:attributes][:name]).to be_a(String)
-
-      expect(item[:data][:attributes]).to have_key(:description)
-      expect(item[:data][:attributes][:description]).to be_a(String)
-
-      expect(item[:data][:attributes]).to have_key(:unit_price)
-      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
-
-      expect(item[:data][:attributes]).to have_key(:merchant_id)
-      expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
-
-      expect(item[:data]).to_not have_key(:created_at)
-      expect(item[:data]).to_not have_key(:updated_at)
+        expect(items[:data]).to be_an(Array)
+      end
     end
+  end
 
-    it 'can create a new item' do
-      merchant = create(:merchant)
-      item_params = {
-        name: 'Motivational Cross Stitch',
-        description: 'Handmade cross stich picture with a motivational quote',
-        unit_price: 5.99,
-        merchant_id: merchant.id
-      }
-
-      headers = { 'CONTENT_TYPE' => 'application/json' }
-
-      post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
-
-      created_item = Item.last
-
-      expect(response).to have_http_status(201)
-
-      expect(created_item.name).to eq(item_params[:name])
-      expect(created_item.description).to eq(item_params[:description])
-      expect(created_item.unit_price).to eq(item_params[:unit_price])
-      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
-    end
-
-    it 'can destroy an item' do
-      id = create(:item).id
-
-      expect(Item.count).to eq(1)
-
-      delete "/api/v1/items/#{id}"
-
-      expect(response).to be_successful
-
-      expect(Item.count).to eq(0)
-      expect { Item.find(id) }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-
-    describe 'can update an existing item' do
-      it 'can update only one part of an items information' do
+  describe 'show item' do
+    describe 'happy path' do
+      it 'can get a single item by its id' do
         id = create(:item).id
-        previous_name = Item.last.name
 
-        update_params = { name: 'Silver necklace' }
-        headers = { 'CONTENT_TYPE' => 'application/json' }
+        get "/api/v1/items/#{id}"
 
-        patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: update_params })
-        item = Item.find(id)
+        item = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
-        expect(item.name).to_not eq(previous_name)
-        expect(item.name).to eq('Silver necklace')
-      end
 
-      it 'can update multiple peices of item information' do
-        id = create(:item).id
-        previous_info = Item.last
+        expect(item).to have_key(:data)
+        expect(item[:data]).to be_a(Hash)
 
-        update_params = {
-          name: 'Silver necklace',
-          unit_price: 10.50
-        }
-        headers = { 'CONTENT_TYPE' => 'application/json' }
+        expect(item[:data]).to have_key(:id)
+        expect(item[:data][:id]).to be_a(String)
 
-        patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: update_params })
-        item = Item.find(id)
+        expect(item[:data]).to have_key(:type)
+        expect(item[:data][:type]).to be_a(String)
 
-        expect(response).to be_successful
-        expect(item.name).to_not eq(previous_info.name)
-        expect(item.name).to eq('Silver necklace')
-        expect(item.unit_price).to_not eq(previous_info.unit_price)
-        expect(item.unit_price).to eq(10.50)
-      end
+        expect(item[:data]).to have_key(:attributes)
+        expect(item[:data][:attributes]).to be_a(Hash)
 
-      it "can update all attributes of an item's information" do
-        item_id = create(:item).id
-        merchant_id = create(:merchant).id
-        previous_info = Item.last
+        expect(item[:data][:attributes]).to have_key(:name)
+        expect(item[:data][:attributes][:name]).to be_a(String)
 
-        update_params = {
-          name: 'Silver necklace',
-          description: '18 inch brushed silver necklace chain with pendent',
-          unit_price: 10.50,
-          merchant_id: merchant_id
-        }
-        headers = { 'CONTENT_TYPE' => 'application/json' }
+        expect(item[:data][:attributes]).to have_key(:description)
+        expect(item[:data][:attributes][:description]).to be_a(String)
 
-        patch "/api/v1/items/#{item_id}", headers: headers, params: JSON.generate({ item: update_params })
-        item = Item.find(item_id)
+        expect(item[:data][:attributes]).to have_key(:unit_price)
+        expect(item[:data][:attributes][:unit_price]).to be_a(Float)
 
-        expect(response).to be_successful
-        expect(item.name).to_not eq(previous_info.name)
-        expect(item.name).to eq('Silver necklace')
+        expect(item[:data][:attributes]).to have_key(:merchant_id)
+        expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
 
-        expect(item.description).to_not eq(previous_info.description)
-        expect(item.description).to eq('18 inch brushed silver necklace chain with pendent')
-
-        expect(item.unit_price).to_not eq(previous_info.unit_price)
-        expect(item.unit_price).to eq(10.50)
-
-        expect(item.merchant_id).to_not eq(previous_info.merchant_id)
-        expect(item.merchant_id).to eq(merchant_id)
+        expect(item[:data]).to_not have_key(:created_at)
+        expect(item[:data]).to_not have_key(:updated_at)
       end
     end
 
-    it 'gets the merchant data for a given item' do
-      id = create(:item).id
+    describe 'sad path' do
+      it 'returns a 404 status code and a empty item hash when an invalid item id is requested' do
+        get '/api/v1/items/1'
 
-      get "/api/v1/items/#{id}/merchant"
+        item = JSON.parse(response.body, symbolize_names: true)
 
-      item_merchant = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to have_http_status(404)
 
-      expect(response).to be_successful
+        expect(item).to have_key(:status)
+        expect(item[:status]).to be_a(String)
 
-      expect(item_merchant).to have_key(:data)
-      expect(item_merchant[:data]).to be_a(Hash)
+        expect(item).to have_key(:error)
+        expect(item[:error]).to be_a(Hash)
 
-      expect(item_merchant[:data]).to have_key(:id)
-      expect(item_merchant[:data][:id]).to be_a(String)
+        expect(item[:error]).to have_key(:id)
+        expect(item[:error][:id]).to be(nil)
 
-      expect(item_merchant[:data]).to have_key(:type)
-      expect(item_merchant[:data][:type]).to be_a(String)
+        expect(item[:error]).to have_key(:type)
+        expect(item[:error][:type]).to be_a(String)
+      end
+    end
+  end
 
-      expect(item_merchant[:data]).to have_key(:attributes)
-      expect(item_merchant[:data][:attributes]).to be_a(Hash)
+  describe 'create item' do
+    describe 'happy path' do
 
-      expect(item_merchant[:data][:attributes]).to have_key(:name)
-      expect(item_merchant[:data][:attributes][:name]).to be_a(String)
+      it 'can create a new item' do
+        merchant = create(:merchant)
+        item_params = {
+          name: 'Motivational Cross Stitch',
+          description: 'Handmade cross stich picture with a motivational quote',
+          unit_price: 5.99,
+          merchant_id: merchant.id
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+
+        created_item = Item.last
+
+        expect(response).to have_http_status(201)
+
+        expect(created_item.name).to eq(item_params[:name])
+        expect(created_item.description).to eq(item_params[:description])
+        expect(created_item.unit_price).to eq(item_params[:unit_price])
+        expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+      end
+    end
+
+    describe 'sad path' do
+      it
+    end
+  end
+
+  describe 'destroy item' do
+    describe'happy path' do 
+
+      it 'can destroy an item' do
+        id = create(:item).id
+
+        expect(Item.count).to eq(1)
+
+        delete "/api/v1/items/#{id}"
+
+        expect(response).to be_successful
+
+        expect(Item.count).to eq(0)
+        expect { Item.find(id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    describe 'sad path' do
+      it
+    end
+  end
+
+  describe 'update item' do
+    describe 'happy path' do
+      describe 'can update an existing item' do
+        it 'can update only one part of an items information' do
+          id = create(:item).id
+          previous_name = Item.last.name
+
+          update_params = { name: 'Silver necklace' }
+          headers = { 'CONTENT_TYPE' => 'application/json' }
+
+          patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: update_params })
+          item = Item.find(id)
+
+          expect(response).to be_successful
+          expect(item.name).to_not eq(previous_name)
+          expect(item.name).to eq('Silver necklace')
+        end
+
+        it 'can update multiple peices of item information' do
+          id = create(:item).id
+          previous_info = Item.last
+
+          update_params = {
+            name: 'Silver necklace',
+            unit_price: 10.50
+          }
+          headers = { 'CONTENT_TYPE' => 'application/json' }
+
+          patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: update_params })
+          item = Item.find(id)
+
+          expect(response).to be_successful
+          expect(item.name).to_not eq(previous_info.name)
+          expect(item.name).to eq('Silver necklace')
+          expect(item.unit_price).to_not eq(previous_info.unit_price)
+          expect(item.unit_price).to eq(10.50)
+        end
+
+        it "can update all attributes of an item's information" do
+          item_id = create(:item).id
+          merchant_id = create(:merchant).id
+          previous_info = Item.last
+
+          update_params = {
+            name: 'Silver necklace',
+            description: '18 inch brushed silver necklace chain with pendent',
+            unit_price: 10.50,
+            merchant_id: merchant_id
+          }
+          headers = { 'CONTENT_TYPE' => 'application/json' }
+
+          patch "/api/v1/items/#{item_id}", headers: headers, params: JSON.generate({ item: update_params })
+          item = Item.find(item_id)
+
+          expect(response).to be_successful
+          expect(item.name).to_not eq(previous_info.name)
+          expect(item.name).to eq('Silver necklace')
+
+          expect(item.description).to_not eq(previous_info.description)
+          expect(item.description).to eq('18 inch brushed silver necklace chain with pendent')
+
+          expect(item.unit_price).to_not eq(previous_info.unit_price)
+          expect(item.unit_price).to eq(10.50)
+
+          expect(item.merchant_id).to_not eq(previous_info.merchant_id)
+          expect(item.merchant_id).to eq(merchant_id)
+        end
+      end
+    end
+
+    describe 'sad path' do
+      it
+    end
+  end
+
+  describe 'item merchant' do
+    describe 'happy path' do
+      it 'gets the merchant data for a given item' do
+        id = create(:item).id
+
+        get "/api/v1/items/#{id}/merchant"
+
+        item_merchant = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(item_merchant).to have_key(:data)
+        expect(item_merchant[:data]).to be_a(Hash)
+
+        expect(item_merchant[:data]).to have_key(:id)
+        expect(item_merchant[:data][:id]).to be_a(String)
+
+        expect(item_merchant[:data]).to have_key(:type)
+        expect(item_merchant[:data][:type]).to be_a(String)
+
+        expect(item_merchant[:data]).to have_key(:attributes)
+        expect(item_merchant[:data][:attributes]).to be_a(Hash)
+
+        expect(item_merchant[:data][:attributes]).to have_key(:name)
+        expect(item_merchant[:data][:attributes][:name]).to be_a(String)
+      end
+    end
+
+    describe 'sad path' do
+      it
     end
   end
 end
