db4e0311d98fad20de80cac891b3e01de4853dde
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-18 20:37:54 -0600

    Refactor: refactor method to use Item Serializer

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,11 +1,13 @@
-class Api::V1::MerchantItemsController < ApplicationController 
+class Api::V1::MerchantItemsController < ApplicationController
+
   def index
     if Merchant.exists?(id: params[:merchant_id])
       merchant = Merchant.find(params[:merchant_id])
-      render json: MerchantSerializer.items_for_merchant(merchant)
+      render json: ItemSerializer.all_items(merchant.items)
     else
       render json:MerchantSerializer.no_merchant_items, status: :not_found
     end
   end
+  
  end
 
