5423926d3194cb1ec737c4eb9d859a6efae0d6ea
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-20 11:28:02 -0600

    added invoice and invoice items facotories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -9,4 +9,16 @@ FactoryBot.define do
     unit_price { Faker::Number.within(range: 500..2000) }
     association :merchant, factory: :merchant
   end
+
+  factory :invoice, class: Invoice do
+    status {Faker::Number.within(range: 0..2)}
+  end
+
+  factory :invoice_items, class: InvoiceItem do
+    quantity {Faker::Number.within(range: 1..20)}
+    unit_price {Faker::Number.within(range: 500..2000)}
+    status {Faker::Number.within(range: 0..2)}
+    association :invoice, factory: :invoice
+    association :item, factory: :item
+  end
 end
