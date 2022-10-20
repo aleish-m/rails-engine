07f04e02a921760d6c82aa2577064400646bf49d
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-20 15:39:31 -0600

    feat: write methods to find and delete invoice that only have the one item being deleted on them

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,13 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant 
-  has_many :transactions
-  has_many :invoice_items 
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy 
   has_many :items, through: :invoice_items
+
+  def self.find_invoices_with_one_item
+    Invoice.joins(:items)
+    .group(:id)
+    .having('count(invoice_items.item_id) = 1')
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
-
+  
   validates_presence_of :name, :description, :unit_price
-  validates :unit_price, numericality: true
+  validates :unit_price, numericality:
+  
+  before_destroy(:delete_invoices, prepend: true)
+  
+  private
+
+  def delete_invoices
+    invoices.find_invoices_with_one_item.each {|invoice| invoice.destroy}
+  end
 end
